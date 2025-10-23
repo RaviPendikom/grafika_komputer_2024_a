833688031737a1d0b5885308e6d9410b976025f2
RaviPendikom <ramaulvi@fkip.unmul.ac.id>
2025-10-23 13:54:57 +0800

    Draw Explained

--- sketch.js
+++ sketch.js
@@ -1,4 +1,10 @@
 function setup() {
   createCanvas(600, 600);
+}
+
+function draw() {
   background(49, 21, 80);
+  noStroke();
+  fill(random(255), random(255), random(255), random(255));
+  circle(300, 300, 300);
 }
