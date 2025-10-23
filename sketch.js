8e5c8c4e54e7ac2fd3698900db435b8dbef3ec96
RaviPendikom <ramaulvi@fkip.unmul.ac.id>
2025-10-23 14:35:58 +0800

    Bola Mantul berwarna dengan ukuran kecil

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 let x = 300;
 let y = 300;
 let speedX = 5;
-let speedY = 2;
+let speedY = 8;
 function setup() {
   createCanvas(600, 600);
   background(49, 21, 80);
@@ -10,7 +10,7 @@ function setup() {
 function draw() {
   noStroke();
   fill(random(255), random(255), random(255), random(255));
-  circle(x, y, 100);
+  circle(x, y, random(50));
   x += speedX;
   y += speedY;
   if (x + 50 >= width || x - 50 <= 0) {
