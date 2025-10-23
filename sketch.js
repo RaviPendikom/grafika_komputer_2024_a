c46e7d930d6dc4ac4389a43fda9787b93885fffe
RaviPendikom <ramaulvi@fkip.unmul.ac.id>
2025-10-23 14:32:45 +0800

    Bola Mantul berwarna

--- sketch.js
+++ sketch.js
@@ -1,16 +1,22 @@
 let x = 300;
 let y = 300;
 let speedX = 5;
+let speedY = 2;
 function setup() {
   createCanvas(600, 600);
+  background(49, 21, 80);
 }
 
 function draw() {
-  background(49, 21, 80);
   noStroke();
+  fill(random(255), random(255), random(255), random(255));
   circle(x, y, 100);
   x += speedX;
-  if (x + 50 >= width) {
+  y += speedY;
+  if (x + 50 >= width || x - 50 <= 0) {
     speedX *= -1;
+  }
+  if (y + 50 >= width || y - 50 <= 0) {
+    speedY *= -1;
   }
 }
