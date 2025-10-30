7a186025cc1ed5d52063c30f829c4122bff4173a
RaviPendikom <ramaulvi@fkip.unmul.ac.id>
2025-10-30 12:56:16 +0800

    membuat line berwarna

--- sketch.js
+++ sketch.js
@@ -1,4 +1,11 @@
+// Click and drag the mouse to view the scene from different angles.
+
 function setup() {
   createCanvas(600, 600);
-  background(49, 21, 80);
+  background(89, 180, 78);
+}
+
+function draw() {
+  stroke(random(255), random(255), random(255));
+  line(300, 300, mouseX, mouseY);
 }
